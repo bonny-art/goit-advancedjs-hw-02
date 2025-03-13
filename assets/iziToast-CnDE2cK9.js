4a5ff3f2174c3b0cf58f56ae294e274fd087cadc
bonny-art <134740924+bonny-art@users.noreply.github.com>
2025-03-13 23:45:41 +0000

    Deploying to gh-pages from @ bonny-art/goit-advancedjs-hw-02@dc5871ee0f1413005719f8ab1582b05a4ed14a25 🚀

--- assets/iziToast-CnDE2cK9.js
+++
@@ -1,2 +0,0 @@
-import{i}from"./vendor-BbbuE1sJ.js";i.settings({timeout:3e5,transitionIn:"flipInX",transitionOut:"flipOutX",iconColor:"#FFF",color:"#FFF",close:!0,position:"topRight",messageColor:"#FFF",messageSize:"16px",progressBar:!0,progressBarEasing:"linear"});
-//# sourceMappingURL=iziToast-CnDE2cK9.js.map
